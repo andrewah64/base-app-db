2f8f3f3aff464ac5c529a0fd2ded259154a80b0c
andrewah64 <andrewah64@gmail.com>
2025-09-09 21:13:27 +0100

    c

--- deploy/function_all_core_unauth_tnt_all_inf_tnt_inf.sql
+++ deploy/function_all_core_unauth_tnt_all_inf_tnt_inf.sql
@@ -14,7 +14,11 @@ begin
                   , tnt.tnt_prtc
                   , tnt.tnt_fqdn
                   , tnt.tnt_port
-                  , tnt.tnt_prtc || '://' || tnt.tnt_fqdn || ':' || tnt.tnt_port::text tnt_origin
+                  , tnt.tnt_prtc || '://' || tnt.tnt_fqdn || case tnt.tnt_port::text
+                                                                 when '443' then ''
+                                                                 when '80'  then ''
+                                                                 else ':' || tnt.tnt_port::text
+                                                             end                                tnt_origin
                from
                     app_data.tenant tnt
                   ;
