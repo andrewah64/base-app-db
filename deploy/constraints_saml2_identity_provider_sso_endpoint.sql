b496aee025b739dbf54ce6db95466d8c9c82f670
andrewah64 <andrewah64@gmail.com>
2025-10-01 22:39:01 +0100

    Fix typo

--- deploy/constraints_saml2_identity_provider_sso_endpoint.sql
+++ deploy/constraints_saml2_identity_provider_sso_endpoint.sql
@@ -1,2 +1,2 @@
 alter table if exists app_data.saml2_identity_provider_sso_endpoint add constraint fk_sso_idp foreign key (idp_id) references app_data.saml2_identity_provider (idp_id);
-alter table if exists app_data.saml2_identity_provider_slo_endpoint add constraint fk_sso_bnd foreign key (bnd_id) references app_data.saml2_endpoint_binding (bnd_id);
+alter table if exists app_data.saml2_identity_provider_sso_endpoint add constraint fk_sso_bnd foreign key (bnd_id) references app_data.saml2_endpoint_binding (bnd_id);
