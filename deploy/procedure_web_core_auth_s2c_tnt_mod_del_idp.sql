a8ac39e60a9686641f64adceb194ae75d9669a09
andrewah64 <andrewah64@gmail.com>
2025-10-03 05:03:37 +0100

    Delete IdPs

--- deploy/procedure_web_core_auth_s2c_tnt_mod_del_idp.sql
+++ deploy/procedure_web_core_auth_s2c_tnt_mod_del_idp.sql
@@ -54,6 +54,21 @@ begin
 
         delete
           from
+               app_data.saml2_identity_provider_certificate ipc
+         where
+               ipc.idp_id = any(p_idp_id)
+           and exists (
+                          select
+                                 null
+                            from
+                                 app_data.saml2_identity_provider idp
+                           where
+                                 idp.idp_id = ipc.idp_id
+                             and idp.tnt_id = p_tnt_id
+                      );
+
+        delete
+          from
                app_data.saml2_identity_provider idp
          where
                idp.tnt_id = p_tnt_id
