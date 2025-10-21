b311e14a6f6367bd81ab0815f6540829fdca3824
andrewah64 <andrewah64@gmail.com>
2025-10-21 22:37:28 +0100

    info for sp-initiated saml2 auth

---
+++ revert/function_web_core_unauth_ssn_aur_reg_s2s_inf.sql
@@ -0,0 +1,1 @@
+drop function if exists web_core_unauth_ssn_aur_reg.s2s_inf;
---
+++ verify/function_web_core_unauth_ssn_aur_reg_s2s_inf.sql
@@ -0,0 +1,7 @@
+do $$
+begin
+        assert(select true
+                 from information_schema.routines
+                where routine_schema = 'web_core_unauth_ssn_aur_reg'
+                  and routine_name   = 's2s_inf');
+end$$
