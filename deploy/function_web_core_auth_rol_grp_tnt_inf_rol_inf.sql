337fa370a28101f67af7b9a04cf47fbf82c00e9d
andrewah64 <andrewah64@gmail.com>
2025-10-23 11:03:52 +0100

    Bring out page name against roles

--- deploy/function_web_core_auth_rol_grp_tnt_inf_rol_inf.sql
+++ deploy/function_web_core_auth_rol_grp_tnt_inf_rol_inf.sql
@@ -14,80 +14,92 @@ begin
              $1
          for
                select
-                      dbrl_id
+                      pg_nm
+                    , dbrl_id
                     , dbrl_ds
                     , grp_has
-                 from           (
-                                    select
-                                           dbrl.dbrl_id
-                                         , dbrl.dbrl_ds
-                                         , true         grp_has
-                                      from
-                                                app_data.db_role     dbrl
-                                           join app_data.atn_db_role adr  on dbrl.dbrl_id = adr.dbrl_id
-                                     where
-                                           dbrl.dbrl_md = false
-                                       and     exists (
-                                                          select
-                                                                 null
-                                                            from
-                                                                      app_data.app_group         grp
-                                                                 join app_data.app_group_db_role grpdr on grp.grp_id = grpdr.grp_id
-                                                           where
-                                                                 grp.tnt_id    = p_tnt_id
-                                                             and grp.grp_id    = p_grp_id
-                                                             and grpdr.dbrl_id = dbrl.dbrl_id
-                                                      )
-                                       and     exists (
-                                                          select
-                                                                 null
-                                                            from
-                                                                      app_data.app_user          aur
-                                                                 join app_data.app_group_user    agu   on aur.aur_id = agu.aur_id
-                                                                 join app_data.app_group_db_role grpdr on agu.grp_id = grpdr.grp_id
-                                                           where
-                                                                 aur.tnt_id    = p_tnt_id
-                                                             and aur.aur_id    = p_aur_id
-                                                             and grpdr.dbrl_id = dbrl.dbrl_id
-                                                      )
-                                )
-                      union all
-                                (
-                                    select
-                                           dbrl.dbrl_id
-                                         , dbrl.dbrl_ds
-                                         , false        grp_has
-                                      from
-                                                app_data.db_role     dbrl
-                                           join app_data.atn_db_role adr  on dbrl.dbrl_id = adr.dbrl_id
-                                     where
-                                           dbrl.dbrl_md = false
-                                       and not exists (
-                                                          select
-                                                                 null
-                                                            from
-                                                                      app_data.app_group         grp
-                                                                 join app_data.app_group_db_role grpdr on grp.grp_id = grpdr.grp_id
-                                                           where
-                                                                 grp.tnt_id    = p_tnt_id
-                                                             and grp.grp_id    = p_grp_id
-                                                             and grpdr.dbrl_id = dbrl.dbrl_id
-                                                      )
-                                       and     exists (
-                                                          select
-                                                                 null
-                                                            from
-                                                                      app_data.app_user          aur
-                                                                 join app_data.app_group_user    agu   on aur.aur_id = agu.aur_id
-                                                                 join app_data.app_group_db_role grpdr on agu.grp_id = grpdr.grp_id
-                                                           where
-                                                                 aur.tnt_id    = p_tnt_id
-                                                             and aur.aur_id    = p_aur_id
-                                                             and grpdr.dbrl_id = dbrl.dbrl_id
-                                                      )
-                                )
+                 from       (
+                                select
+                                       pg.pg_nm
+                                     , dbrl.dbrl_id
+                                     , dbrl.dbrl_ds
+                                     , true         grp_has
+                                  from
+                                            app_data.page             pg
+                                       join app_data.page_endpoint    pe   on pg.pg_id     = pe.pg_id
+                                       join app_data.endpoint         ep   on pe.ep_id     = ep.ep_id
+                                       join app_data.endpoint_db_role edr  on ep.ep_id     = edr.ep_id
+                                       join app_data.db_role          dbrl on edr.dbrl_id  = dbrl.dbrl_id
+                                       join app_data.atn_db_role      adr  on dbrl.dbrl_id = adr.dbrl_id
+                                 where
+                                       dbrl.dbrl_md = false
+                                   and     exists (
+                                                      select
+                                                             null
+                                                        from
+                                                                  app_data.app_group         grp
+                                                             join app_data.app_group_db_role grpdr on grp.grp_id = grpdr.grp_id
+                                                       where
+                                                             grp.tnt_id    = p_tnt_id
+                                                         and grp.grp_id    = p_grp_id
+                                                         and grpdr.dbrl_id = dbrl.dbrl_id
+                                                  )
+                                   and     exists (
+                                                      select
+                                                             null
+                                                        from
+                                                                  app_data.app_user          aur
+                                                             join app_data.app_group_user    agu   on aur.aur_id = agu.aur_id
+                                                             join app_data.app_group_db_role grpdr on agu.grp_id = grpdr.grp_id
+                                                       where
+                                                             aur.tnt_id    = p_tnt_id
+                                                         and aur.aur_id    = p_aur_id
+                                                         and grpdr.dbrl_id = dbrl.dbrl_id
+                                                  )
+                            )
+                      union
+                            (
+                                select
+                                       pg.pg_nm
+                                     , dbrl.dbrl_id
+                                     , dbrl.dbrl_ds
+                                     , false        grp_has
+                                  from
+                                            app_data.page             pg
+                                       join app_data.page_endpoint    pe   on pg.pg_id     = pe.pg_id
+                                       join app_data.endpoint         ep   on pe.ep_id     = ep.ep_id
+                                       join app_data.endpoint_db_role edr  on ep.ep_id     = edr.ep_id
+                                       join app_data.db_role          dbrl on edr.dbrl_id  = dbrl.dbrl_id
+                                       join app_data.atn_db_role      adr  on dbrl.dbrl_id = adr.dbrl_id
+                                 where
+                                       dbrl.dbrl_md = false
+                                   and not exists (
+                                                      select
+                                                             null
+                                                        from
+                                                                  app_data.app_group         grp
+                                                             join app_data.app_group_db_role grpdr on grp.grp_id = grpdr.grp_id
+                                                       where
+                                                             grp.tnt_id    = p_tnt_id
+                                                         and grp.grp_id    = p_grp_id
+                                                         and grpdr.dbrl_id = dbrl.dbrl_id
+                                                  )
+                                   and     exists (
+                                                      select
+                                                             null
+                                                        from
+                                                                  app_data.app_user          aur
+                                                             join app_data.app_group_user    agu   on aur.aur_id = agu.aur_id
+                                                             join app_data.app_group_db_role grpdr on agu.grp_id = grpdr.grp_id
+                                                       where
+                                                             aur.tnt_id    = p_tnt_id
+                                                         and aur.aur_id    = p_aur_id
+                                                         and grpdr.dbrl_id = dbrl.dbrl_id
+                                                  )
+                            )
              order by
-                      dbrl_ds asc
+                      pg_nm   asc
+                    , dbrl_ds asc
                     ;
 
         return $1;
